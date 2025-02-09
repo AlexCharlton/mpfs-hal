5b2a31dc6df0f1ce71ea3580bb98d9248ccdc233
Alex Charlton <alex.n.charlton@gmail.com>
2025-02-08 22:54:24 -0800

    flasher: generate image based on elf entry point

--- hss-tty-flasher/src/image_gen.rs
+++ hss-tty-flasher/src/image_gen.rs
@@ -8,8 +8,33 @@ pub fn generate_hss_payload(elf_path: &str) -> std::io::Result<String> {
     let file_name = path.file_stem().unwrap().to_str().unwrap();
     let dir = path.parent().unwrap_or(Path::new("."));
 
+    let file_data = std::fs::read(path).unwrap();
+    let elf = elf::ElfBytes::<elf::endian::AnyEndian>::minimal_parse(file_data.as_slice()).unwrap();
+    let entry_point = elf.ehdr.e_entry;
+
     // Create the yaml content using the CONFIG template
-    let yaml_content = CONFIG.replace("{}", elf_name);
+    let yaml_content = format!(
+        "
+set-name: 'PolarFire-SoC-HSS::{elf_name}'
+
+hart-entry-points:
+  u54_1: '{entry_point:#x}'
+  u54_2: '{entry_point:#x}'
+  u54_3: '{entry_point:#x}'
+  u54_4: '{entry_point:#x}'
+
+payloads:
+  {elf_name}:
+    owner-hart: u54_1
+    secondary-hart: u54_2
+    secondary-hart: u54_3
+    secondary-hart: u54_4
+    priv-mode: prv_m
+    skip-opensbi: true
+",
+        elf_name = elf_name,
+        entry_point = entry_point
+    );
     let yaml_filename = format!("{}-image-conf.yaml", file_name);
     let yaml_path = dir.join(&yaml_filename);
     fs::write(&yaml_path, yaml_content)?;
@@ -33,22 +58,3 @@ pub fn generate_hss_payload(elf_path: &str) -> std::io::Result<String> {
 
     Ok(image_path.to_string_lossy().into_owned())
 }
-
-const CONFIG: &str = r#"
-set-name: 'PolarFire-SoC-HSS::{}'
-
-hart-entry-points:
-  u54_1: '0x1000000000'
-  u54_2: '0x1000000000'
-  u54_3: '0x1000000000'
-  u54_4: '0x1000000000'
-
-payloads:
-  {}:
-    owner-hart: u54_1
-    secondary-hart: u54_2
-    secondary-hart: u54_3
-    secondary-hart: u54_4
-    priv-mode: prv_m
-    skip-opensbi: true
-"#;
