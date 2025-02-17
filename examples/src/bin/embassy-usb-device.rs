76e9de22c80bc7f8ebad8d1f3e12fe330acd72bf
Alex Charlton <alex.n.charlton@gmail.com>
2025-02-17 12:07:32 -0800

    HID example: use embassy_usb

--- examples/src/bin/embassy-usb-device.rs
+++ examples/src/bin/embassy-usb-device.rs
@@ -1,6 +1,14 @@
 #![no_std]
 #![no_main]
 
+use aligned::{Aligned, A4};
+use embassy_futures::join::join;
+use embassy_time::Timer;
+use embassy_usb::class::hid::{HidWriter, ReportId, RequestHandler, State};
+use embassy_usb::control::OutResponse;
+use embassy_usb::Builder;
+use usbd_hid::descriptor::{MouseReport, SerializedDescriptor};
+
 use mpfs_hal::Peripheral;
 
 #[macro_use]
@@ -8,29 +16,94 @@ extern crate mpfs_hal;
 
 #[mpfs_hal_embassy::embassy_hart1_main]
 async fn hart1_main(_spawner: embassy_executor::Spawner) {
-    let _usb_driver = mpfs_hal::usb::device::UsbDriver::take().unwrap();
-    println!("Hello, world!\nWe're going to wiggle your mouse along the X axis 🖱 ️↔️ ");
-    loop {
-        // unsafe {
-        //     if pac::MSS_USBD_HID_tx_done() == 1 {
-        //         #[allow(static_mut_refs)]
-        //         pac::MSS_USBD_HID_tx_report(
-        //             &REPORT as *const _ as *mut u8,
-        //             core::mem::size_of::<pac::mss_usbd_hid_report_t>() as u32,
-        //         );
-        //     } else {
-        //         // Delay
-        //         for _ in 0..50000 {
-        //             core::hint::spin_loop();
-        //         }
+    let driver = mpfs_hal::usb::device::UsbDriver::take().unwrap();
+    // Create embassy-usb Config
+    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("HID mouse example");
+    config.serial_number = Some("12345678");
 
-        //         if REPORT.x_move < 30 {
-        //             REPORT.x_move += 1;
-        //         } else {
-        //             REPORT.x_move = -30;
-        //         }
-        //     }
-        // }
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors, and they should be 4-byte aligned.
+    let mut config_descriptor = Aligned::<A4, _>([0; 256]);
+    let mut bos_descriptor = Aligned::<A4, _>([0; 256]);
+    let mut control_buf = Aligned::<A4, _>([0; 64]);
+
+    let mut request_handler = MyRequestHandler {};
+
+    let mut state = State::new();
+
+    let mut builder = Builder::new(
+        driver,
+        config,
+        config_descriptor.as_mut_slice(),
+        bos_descriptor.as_mut_slice(),
+        &mut [], // no msos descriptors
+        control_buf.as_mut_slice(),
+    );
+
+    // Create classes on the builder.
+    let config = embassy_usb::class::hid::Config {
+        report_descriptor: MouseReport::desc(),
+        request_handler: Some(&mut request_handler),
+        poll_ms: 60,
+        max_packet_size: 8,
+    };
+
+    let mut writer = HidWriter::<_, 5>::new(&mut builder, &mut state, config);
+
+    // Build the builder.
+    let mut usb = builder.build();
+
+    // Run the USB device.
+    let usb_fut = usb.run();
+
+    // Do stuff with the class!
+    let hid_fut = async {
+        let mut y: i8 = 5;
+        loop {
+            Timer::after_millis(500).await;
+
+            y = -y;
+            let report = MouseReport {
+                buttons: 0,
+                x: 0,
+                y,
+                wheel: 0,
+                pan: 0,
+            };
+            match writer.write_serialize(&report).await {
+                Ok(()) => {}
+                Err(e) => log::warn!("Failed to send report: {:?}", e),
+            }
+        }
+    };
+
+    // Run everything concurrently.
+    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
+    join(usb_fut, hid_fut).await;
+    println!("Hello, world!\nWe're going to wiggle your mouse along the X axis 🖱 ️↔️ ");
+}
+struct MyRequestHandler {}
+
+impl RequestHandler for MyRequestHandler {
+    fn get_report(&mut self, id: ReportId, _buf: &mut [u8]) -> Option<usize> {
+        log::info!("Get report for {:?}", id);
+        None
+    }
+
+    fn set_report(&mut self, id: ReportId, data: &[u8]) -> OutResponse {
+        log::info!("Set report for {:?}: {:x?}", id, data);
+        OutResponse::Accepted
+    }
+
+    fn set_idle_ms(&mut self, id: Option<ReportId>, dur: u32) {
+        log::info!("Set idle rate for {:?} to {:?}", id, dur);
+    }
+
+    fn get_idle_ms(&mut self, id: Option<ReportId>) -> Option<u32> {
+        log::info!("Get idle rate for {:?}", id);
+        None
     }
 }
 
